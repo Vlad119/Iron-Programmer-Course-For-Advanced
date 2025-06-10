6e89824e94491b961c28727be6695cf86ae35e78
Vlad119 <unstarkble12@gmail.com>
2025-06-10 16:03:46 +0300

    Сделал задачу 4_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Исходное значение переменной: " + number);
+            NextEvenNumber(ref number);
+            Console.WriteLine("Измененное значение переменной: " + number);
+        }
 
+        static void NextEvenNumber(ref int _number)
+        {
+            if (_number % 2 == 0)
+            {
+                _number += 2;
+            }
+            else
+            {
+                _number += 1;
+            }
         }
     }
 }
