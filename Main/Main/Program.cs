07b522e23de681b6fcfe4d70271c2ba2aa2f2799
Vlad119 <unstarkble12@gmail.com>
2025-06-10 16:09:43 +0300

    Решил задачу 4_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double enemySpeed = 5.5;
+            double playerSpeed = 10.2;
+            SwapSpeeds(ref enemySpeed, ref playerSpeed);
+            Console.WriteLine($"{enemySpeed} и {playerSpeed}");
+        }
 
+        static void SwapSpeeds(ref double enemySpeed, ref double playerSpeed)
+        {
+            double temp = enemySpeed;
+            enemySpeed = playerSpeed;
+            playerSpeed = temp;
         }
     }
 }
