a6e23bbf031d8192ef73233c8958b48d1271ebfc
Vlad119 <unstarkble12@gmail.com>
2025-06-10 16:19:24 +0300

    Решил задачу 4_1_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            int levelProgres = int.Parse(Console.ReadLine()); // Передаём 5
+            bool isSuccess = ResetProgres(ref levelProgres);
+            Console.WriteLine($"Уровень игрока обнулён с результатом {isSuccess}. Теперь у игрока {levelProgres} уровень");
+        }
+        static bool ResetProgres(ref int levelProgres)
+        {
+            if (levelProgres == 0)
+            {
+                return false;
+            }
+            levelProgres = 0;
+            return true;
         }
     }
 }
