9d2a9d079689fe6dc678edd7d4b84d06e54858c3
Vlad119 <unstarkble12@gmail.com>
2025-06-11 18:37:34 +0300

    Решил задачу 4_1_8

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
+class Program
+{
+    static void Main(string[] args)
+    {
+        double c = 25;
+        bool IsSuccess = ConvertTemperature(c, out double f, out double k);
+        Console.WriteLine($"Температура перешла абсолютный 0 со значением {IsSuccess}. F: {f}, K: {k}");
+    }
+
+    static bool ConvertTemperature(double c, out double f, out double k)
+    {
+        k = c + 273.15;
+        f = (c * 1.8) + 32;
+        if (c <= -273.15) return false;
+        return true;
     }
 }
