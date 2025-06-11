c0ad063f6b2cec2e3da336142aa38bcfe023b131
Vlad119 <unstarkble12@gmail.com>
2025-06-11 19:09:12 +0300

    Решил задачу 4_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine(); 
+            Greet(); // "Привет, Гость!"
+            Greet(name); // "Привет, name!"
+        }
 
+        static void Greet(string name = "Гость")
+        {
+            Console.WriteLine($"Привет, {name}!");
         }
     }
 }
