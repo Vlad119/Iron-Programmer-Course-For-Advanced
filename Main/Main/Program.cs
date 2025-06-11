66dff48fef95b5546518f5771a9c53c42ee143de
Vlad119 <unstarkble12@gmail.com>
2025-06-11 14:47:45 +0300

    Решил задачу 4_1_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Main
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int sides = 5;
+            bool IsSuccess = IsEven(sides, out int rightSides);
+            Console.WriteLine($"Фигура задана {sides} сторонами с результатом {IsSuccess}, фигура создана. У неё: {rightSides} стороны(он)");
+        }
 
+        static bool IsEven(int sides, out int rightSides)
+        {
+            if (sides % 2 == 0 && sides >= 3)
+            {
+                rightSides = sides;
+                return true;
+            }
+            else
+            {
+                rightSides = sides < 3 ? 4 : sides + 1;
+                return false;
+            }
         }
     }
 }
