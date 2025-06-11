dfd03f4cce09d224058b755154a3bac7f54f0d67
Vlad119 <unstarkble12@gmail.com>
2025-06-11 18:53:15 +0300

    Решил задачу 4_1_9

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string format = "Иосиф:Яблочный спас";
+        if (SplitNameAndHoliday(format, out string name, out string holiday))
+        {
+            Console.WriteLine($"{name} поздравляю тебя с праздником под названием: {holiday}");
         }
+        else
+        {
+            Console.WriteLine("Неверный формат праздника");
+        }
+    }
+
+    static bool SplitNameAndHoliday(string input, out string name, out string holiday)
+    {
+        name = string.Empty;
+        holiday = string.Empty;
+        if (string.IsNullOrEmpty(input)) return false;
+        string[] split = input.Split(':');
+        if (split.Length != 2) return false;
+        if (string.IsNullOrWhiteSpace(split[0]) || string.IsNullOrWhiteSpace(split[1])) return false;
+        name = split[0];
+        holiday = split[1];
+        return true;
     }
 }
