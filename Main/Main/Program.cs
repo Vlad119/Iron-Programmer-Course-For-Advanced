1b9a32e0d10dcd7ac7fb4ce2f065bdb355361f8e
Vlad119 <unstarkble12@gmail.com>
2025-06-11 19:04:38 +0300

    Решил задачу 4_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Car("Mercedes-Benz E-Class", "Blue", 2021, "Unknown", true);
+    }
+
+    static void Car(string model, string color, int year = 2024, string engineType = "Unknown", bool hasSunroof = false)
+    {
+        Console.WriteLine($"Model: {model}");
+        Console.WriteLine($"Color: {color}");
+        Console.WriteLine($"Year: {year}");
+        Console.WriteLine($"Engine Type: {engineType}");
+        Console.WriteLine($"Has Sunroof: {hasSunroof}");
+
     }
 }
