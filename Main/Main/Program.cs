f773871a1a2714847c5ddf0d7f676edaa9cd2f12
Vlad119 <unstarkble12@gmail.com>
2025-06-11 19:13:01 +0300

    Решил задачу 4_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int distance = 10;
+            int discount = 50;
+            Console.WriteLine(CalculateDelivery(distance: distance));
+            Console.WriteLine(CalculateDelivery(distance: distance, discount: discount));
+        }
 
+        static double CalculateDelivery(int distance, int discount = 0)
+        {
+            double result = distance * 20 * (1 - ((double)discount / 100));
+            return result;
         }
     }
 }
