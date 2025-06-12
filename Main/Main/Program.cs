c5087ba02f0edfc0796d9b070daef447c6c96340
Vlad119 <unstarkble12@gmail.com>
2025-06-12 17:31:00 +0300

    Решил задачу 4_2_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int width = 5;
+            int height = 4;
+            Console.WriteLine(CreateRectangle(width));
+            Console.WriteLine(CreateRectangle(width, height));
+        }
 
+        static string CreateRectangle(int width, int height = -1)
+        {
+            if (width <= 0 || (height != -1 && height <= 0))
+            {
+                return "Неверный формат";
+            }
+            if (height == -1)
+            {
+                return $"Квадрат {width}x{width}";
+            }
+            return width == height? $"Квадрат {width}x{height}": $"Прямоугольник {width}x{height}";
         }
     }
 }
