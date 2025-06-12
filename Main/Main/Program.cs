250a731ac48c060ea5c632149f83b163512f128f
Vlad119 <unstarkble12@gmail.com>
2025-06-12 17:13:29 +0300

    Решил задачу 4_2_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            SendNotification(message: "Новое сообщение");
+            SendNotification(message: "Новое сообщение", sound: "ping");
+            SendNotification(message: "Важно!", isUrgent: true);
+            SendNotification(message: "Важно!", "ping", isUrgent: true);
+        }
 
+        static void SendNotification(string message, string sound = "default", bool isUrgent = false)
+        {
+            Console.WriteLine($"Получено сообщение {message}, оно воспроизведено со звуком {sound} и параметр срочности равен {isUrgent}");
         }
     }
 }
