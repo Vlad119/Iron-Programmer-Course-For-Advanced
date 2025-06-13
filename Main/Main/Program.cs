98c4caa013ad11ee05799e516aa09a82d6382382
Vlad119 <unstarkble12@gmail.com>
2025-06-13 15:22:18 +0300

    Решил задачу 4_2_9

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string server = "example.com";
+            int port = 9000;
+            int timeout = 60;
+            Connect(server: server); 
+            Connect(server: server, port: port);
+            Connect(server: server, port: port, timeout: timeout); 
+        }
 
+        static void Connect(string server, int port = 8080, int timeout = 30)
+        {
+            Console.WriteLine($"Подключение к {server}:{port}, Таймаут: {timeout} сек");
         }
     }
 }
