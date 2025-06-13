2130faf93a4307fda8c41bd755a22b41ff5404e8
Vlad119 <unstarkble12@gmail.com>
2025-06-13 15:20:49 +0300

    Решил задаыу 4_2_8

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string login = "admin";
+            string email = "admin@site.com";
+            int age = 25;
+            Console.WriteLine(RegisterUser(login)); 
+            Console.WriteLine(RegisterUser(login, email)); 
+            Console.WriteLine(RegisterUser(login, email, age)); 
+        }
 
+        static string RegisterUser(string login, string email = "не указано", int age = 18)
+        {
+            return $"Логин: {login}, Email: {email}, Возраст: {age}";
         }
     }
 }
