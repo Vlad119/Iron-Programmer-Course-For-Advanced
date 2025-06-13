9076aa98bbe7baab72530bcaaf124783746f84e0
Vlad119 <unstarkble12@gmail.com>
2025-06-13 17:14:54 +0300

    Решил задачу 4_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = 3;
+            string[] names = { "Игорь Кузнецов", "Елена Соколова", "Максим Петров" };
+            AddNumbers(names);
+        }
 
+        static void AddNumbers(params string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {names[i]}");
+            }
         }
     }
 }
