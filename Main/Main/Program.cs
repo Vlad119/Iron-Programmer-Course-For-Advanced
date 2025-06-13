d03789147f7b2bfa34148ea32f918d9479bd3149
Vlad119 <unstarkble12@gmail.com>
2025-06-13 15:17:00 +0300

    Решил задачу 4_2_7

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            int b = 3;
+            Console.WriteLine(Calculate(a, b)); // Вывод: 8
+            string operation = "*";
+            Console.WriteLine(Calculate(a, b, operation)); // Вывод: 15
+        }
 
+        static double Calculate(int a, int b, string operation = "+")
+        {
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+
+                case "-":
+                    return a - b;
+
+                case "*":
+                    return a * b;
+
+                case "/":
+                    if (b == 0)
+                    {
+                        return 0;
+                    }
+                    return (double)a / b;
+                default: return a + b;
+            }
         }
     }
 }
