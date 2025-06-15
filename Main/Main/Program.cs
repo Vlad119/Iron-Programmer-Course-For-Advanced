b8fbabadebcfbc40a315f2209aeb9f3cd3619172
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:07:18 +0300

    Решил задачу 9_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Main
+﻿public class Student
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+    public string FirstName = "Неизвестное имя";
+    public string LastName = "Неизвестная фамилия";
 
-        }
+    public Student()
+    {
+        FirstName = "Unknown FirstName";
+        LastName = "Unknown LastName";
+    }
+
+    public string Print()
+    {
+        return FirstName + " " + LastName;
     }
 }
