debd45c5bfbdb2f5b8ac153396668c5f0fa6cc0b
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:47:27 +0300

    Решил задачу 5_2_7

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] words = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            words[i] = Console.ReadLine().Trim();
+        }
+        for (int i = 1; i < n; i++)
+        {
+            char lastChar = char.ToUpper(words[i - 1][words[i - 1].Length - 1]);
+            char firstChar = char.ToUpper(words[i][0]);
+            if (lastChar != firstChar)
+            {
+                int moveNumber = i + 1;
+                if (moveNumber % 2 == 1)
+                {
+                    Console.WriteLine("Паша");
+                }
+                else
+                {
+                    Console.WriteLine("Маша");
+                }
+                return;
+            }
+        }
+        if (n % 2 == 1)
+        {
+            Console.WriteLine("Маша");
+        }
+        else
+        {
+            Console.WriteLine("Паша");
         }
     }
 }
