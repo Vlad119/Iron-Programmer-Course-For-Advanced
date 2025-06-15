8c9c1c896d5b88fa89a85469810b80594365fdac
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:11:56 +0300

    Решил задачу 9_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Dog dog = new Dog("Песик", 2);
+            Console.WriteLine(dog.GetDescription());
+            Console.WriteLine(dog.Speak("Woof Woof"));
+            Console.WriteLine(dog.Speak("Bow Wow"));
+        }
+    }
 
+    public class Dog
+    {
+        public string Name;
+        public int age;
+
+        public Dog(string _name, int _age)
+        {
+            Name = _name;
+            age = _age;
+        }
+
+        public string GetDescription()
+        {
+            string s = $"{Name} is {age} years old";
+            return s;
+        }
+
+        public string Speak(string sound)
+        {
+            string s = $"{Name} says {sound}";
+            return s;
         }
     }
 }
