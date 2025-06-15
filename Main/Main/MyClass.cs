510e9e3dd8c096b40a05b77b8e01b582fc3613eb
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:58:02 +0300

    Решил задачу 9_2_6

---
+++ Main/Main/MyClass.cs
@@ -0,0 +1,27 @@
+﻿public class MyClass
+{
+    public string field1;
+    public int field2;
+    public bool field3;
+
+    public MyClass(string s)
+    {
+        field1 = s;
+        field2 = 0;
+        field3 = false;
+    }
+
+    public MyClass(string s, int a)
+    {
+        field1 = s;
+        field2 = a;
+        field3 = false;
+    }
+
+    public MyClass(string s, int a, bool b)
+    {
+        field1 = s;
+        field2 = a;
+        field3 = b;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace Main
+﻿internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-
-        }
+        MyClass m1 = new MyClass("test");
+        MyClass m2 = new MyClass("test2", 4);
+        MyClass m3 = new MyClass("test3", 4, false);
     }
 }
