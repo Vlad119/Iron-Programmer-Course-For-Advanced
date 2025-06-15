8433910de4df64f75327602765f2613e5503aa04
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:31:47 +0300

    Решил задачу 9_2_2

---
+++ Main/Main/Dog.cs
@@ -0,0 +1,23 @@
+﻿namespace Main
+{
+    public class Dog
+    {
+        public string Name;
+        public int age;
+        public Dog(string _name, int _age)
+        {
+            Name = _name;
+            age = _age;
+        }
+        public string GetDescription()
+        {
+            string s = $"{Name} is {age} years old";
+            return s;
+        }
+        public string Speak(string sound)
+        {
+            string s = $"{Name} says {sound}";
+            return s;
+        }
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -12,28 +12,4 @@ namespace Main
             Console.WriteLine(dog.Speak("Bow Wow"));
         }
     }
-
-    public class Dog
-    {
-        public string Name;
-        public int age;
-
-        public Dog(string _name, int _age)
-        {
-            Name = _name;
-            age = _age;
-        }
-
-        public string GetDescription()
-        {
-            string s = $"{Name} is {age} years old";
-            return s;
-        }
-
-        public string Speak(string sound)
-        {
-            string s = $"{Name} says {sound}";
-            return s;
-        }
-    }
 }
