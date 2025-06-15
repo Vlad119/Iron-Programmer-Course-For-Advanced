168a8180470d5ba1dde7d77ed45d66747aebe208
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:16:25 +0300

    Сделал задачу 9_2_3

---
+++ Main/Main/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Main
+{
+    public class Point
+    {
+        public double x, y;
+
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public string Print()
+        {
+            string s = $"({x},{y})";
+            return s;
+        }
+        public double CalculateDistance(Point otherPoint)
+        {
+            double distance = Math.Sqrt(Math.Pow(x - otherPoint.x, 2) + Math.Pow(y - otherPoint.y, 2));
+            return distance;
+        }
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Point p1 = new Point(1, 2);
+            Console.WriteLine(p1.Print());
+            Point p2 = new Point(4, 6);
+            Console.WriteLine(p2.Print());
+            Console.WriteLine(p1.CalculateDistance(p2));
+            Console.WriteLine(p2.CalculateDistance(p1));
         }
     }
 }
