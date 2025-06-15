0645c292a1955498b95c6927a77b94df0b6ad237
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:56:14 +0300

    Решил задачу 9_2_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
+internal class Program
+{
+    public static void Main()
+    {
+        int[] numbers = new int[] { 11, 12, 13, 14, 15, 16, 22, 44, 66 };
+        Selector selector = new Selector(numbers);
+        selector.PrintOdds();
+        Console.WriteLine();
+        selector.PrintEvens();
     }
 }
---
+++ Main/Main/Selector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Selector
+{
+    public int[] array;
+
+    public Selector(int[] array)
+    {
+        this.array = array;
+    }
+
+    public void PrintOdds()
+    {
+        foreach (var a in array)
+        {
+            if (Math.Abs(a) % 2 != 0) Console.Write(a + " ");
+        }
+    }
+
+    public void PrintEvens()
+    {
+        foreach (var a in array)
+        {
+            if (Math.Abs(a) % 2 == 0) Console.Write(a + " ");
+        }
+    }
+}
