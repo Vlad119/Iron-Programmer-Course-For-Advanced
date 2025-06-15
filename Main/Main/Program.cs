1c9f84fc43e0112baecae786daebb6e8d7d0f361
Vlad119 <unstarkble12@gmail.com>
2025-06-15 21:09:22 +0300

    Решил задачу 9_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Main
+﻿public class Phone
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+    public string Name; 
+    public int Price; 
+    public int Memory; 
+    public bool IsAvailable; 
 
-        }
+    public Phone(string name)
+    {
+        Name = name;
+        Price = 5000;
+        Memory = 8;
+        IsAvailable = true;
+    }
+
+    public Phone(string name, int price)
+    {
+        Name = name;
+        Price = price;
+        Memory = 8;
+        IsAvailable = true;
+    }
+
+    public Phone(string name, int price, int memory)
+    {
+        Name = name;
+        Price = price;
+        Memory = memory;
+        IsAvailable = true;
+    }
+    public Phone(string name, int price, int memory, bool isAvailable)
+    {
+        Name = name;
+        Price = price;
+        Memory = memory;
+        IsAvailable = isAvailable;
     }
 }
---
+++ Main/Main/UpdatedPhone.cs
@@ -0,0 +1,24 @@
+﻿public class UpdatedPhone
+{
+    public string Name; 
+    public int Price; 
+    public int Memory; 
+    public bool IsAvailable; 
+
+    public UpdatedPhone(string name) : this(name, 5000, 8, true)
+    { }
+
+    public UpdatedPhone(string name, int price) : this(name, price, 8, true)
+    { }
+
+    public UpdatedPhone(string name, int price, int memory) : this(name, price, memory, true)
+    { }
+
+    public UpdatedPhone(string name, int price, int memory, bool isAvailable)
+    {
+        Name = name;
+        Price = price;
+        Memory = memory;
+        IsAvailable = isAvailable;
+    }
+}
