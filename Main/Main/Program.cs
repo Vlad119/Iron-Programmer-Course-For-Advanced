502c1c287080a87f720f4e9e0f168523c45ed5fb
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:03:07 +0300

    Решил задачу 5_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        int plus = 0;
+        int pow = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '+') plus++;
+            if (s[i] == '*') pow++;
         }
+        Console.WriteLine("Символ + встречается " + plus + " раз");
+        Console.WriteLine("Символ * встречается " + pow + " раз");
     }
 }
