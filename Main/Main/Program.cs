f3e519a4f57d8344859f80075052bed91340471d
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:46:37 +0300

    Решил задачу 5_2_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsDigit(s[i]))
+            {
+                Console.Write(s[i] + " ");
+            }
         }
     }
 }
