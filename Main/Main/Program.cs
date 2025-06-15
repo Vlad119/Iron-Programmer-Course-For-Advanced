531c97c733247e9bf3807351e2771c1934947839
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:01:55 +0300

    Решил задачу 4_3_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string separator = ", ";
+            Console.WriteLine(JoinNotes(separator, "Eat", "sleep", "code", "repeat"));
+        }
 
+        static string JoinNotes(string separator, params string[] str)
+        {
+            string result = string.Empty;
+            result = string.Join(separator, str);
+            return result;
         }
     }
 }
