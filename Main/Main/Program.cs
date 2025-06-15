9ece9f483f71c04b60aeff148aaf2a23eb1b243a
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:45:07 +0300

    Решил задачу 5_2_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string s1 = Convert.ToString(Console.ReadLine());
+        string s2 = Convert.ToString(Console.ReadLine());
+        char c1 = char.ToLower(s1[0]);
+        char c2 = char.ToLower(s2[s2.Length - 1]);
+        if (c1 == c2)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
         }
     }
 }
