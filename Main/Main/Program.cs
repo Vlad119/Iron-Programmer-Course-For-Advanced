50680a6fbc3683c5086878206cc63b6c9aad4065
Vlad119 <unstarkble12@gmail.com>
2025-06-15 18:49:56 +0300

    Решил задачу 4_3_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = 72;
+            int[] numbers = { 43, 85, 34, 21, 90, 123, 4, 6 };
+            FindMax(n, numbers);
+        }
 
+        static int FindMax(int n, params int[] numbers)
+        {
+            int max = 0; 
+            foreach (int number in numbers)
+            {
+                if (number <= n && number > max) 
+                {
+                    max = number;
+                }
+            }
+            return max; 
         }
     }
 }
