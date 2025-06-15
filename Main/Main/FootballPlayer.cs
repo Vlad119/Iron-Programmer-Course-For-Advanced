b754f50967b1a275ee32d23e79f0b0b32323fbb7
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:53:17 +0300

    Решил задачу 9_2_4

---
+++ Main/Main/FootballPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class FootballPlayer
+{
+    public string name, surname;
+    public int goals, assists;
+
+    public FootballPlayer(string _name, string _surname)
+    {
+        name = _name;
+        surname = _surname;
+        goals = assists = 0;
+    }
+
+    public void AddScore(int _goals)
+    {
+        goals += _goals;
+    }
+
+    public void AddAssists(int _assists)
+    {
+        assists += _assists;
+    }
+
+    public void ShowStatistics()
+    {
+        Console.WriteLine($"{surname} {name} - голы: {goals}, передачи: {assists}");
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Main
+﻿internal class Program
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
+        FootballPlayer player1 = new FootballPlayer("Cristiano", "Ronaldo");
+        player1.AddScore(711);
+        player1.AddAssists(169);
+        player1.ShowStatistics();
 
-        }
+        FootballPlayer player2 = new FootballPlayer("Lionel", "Messi");
+        player2.AddScore(682);
+        player2.AddAssists(240);
+        player2.ShowStatistics();
     }
 }
