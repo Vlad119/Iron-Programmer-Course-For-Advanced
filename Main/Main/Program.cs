5fe6a9f7a6520f8f5cbd7bb75f30fc57ca414070
Vlad119 <unstarkble12@gmail.com>
2025-06-15 20:07:56 +0300

    Решил задачу 9_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace Main
+﻿public class User
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+    public string Name;
+    public string PhoneNumber;
 
-        }
+    public User()
+    {
+        Name = "Поликарп";
+        PhoneNumber = "+7(000)000 00 00";
     }
 }
