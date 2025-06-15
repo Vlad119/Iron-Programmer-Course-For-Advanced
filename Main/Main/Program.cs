fba990bc41f4151c9a8aa5eee0b830e39a2d2d53
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:03:42 +0300

    Решил задачу 5_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        int sosedy = 0;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == s[i + 1])
+            {
+                sosedy++;
+            }
         }
+        Console.WriteLine(sosedy);
     }
 }
