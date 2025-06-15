1c1c571e72fd456b696a79189bd483d31a4f57a6
Vlad119 <unstarkble12@gmail.com>
2025-06-15 19:44:29 +0300

    Решил задачу 5_2_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string s = Convert.ToString(Console.ReadLine());
+        char c = s[0];
+        if (char.IsLetter(c) && char.IsUpper(c))
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
         }
     }
 }
