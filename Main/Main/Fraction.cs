42569cd4b1f9de9a0e02181c7f6f3308715d7696
Vlad119 <unstarkble12@gmail.com>
2025-06-16 21:38:00 +0300

    Решил задачу 10_2

---
+++ Main/Main/Fraction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Fraction
+{
+    public int numerator;
+    public int denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{numerator}/{denominator}");
+    }
+
+    public Fraction Sum(Fraction otherFraction)
+    {
+        int commonDenominator = denominator * otherFraction.denominator;
+        int resultNumerator = numerator * otherFraction.denominator + otherFraction.numerator * denominator;
+        Fraction result = new Fraction(resultNumerator, commonDenominator);
+        return result;
+    }
+    public Fraction Difference(Fraction otherFraction)
+    {
+        int commonDenominator = denominator * otherFraction.denominator;
+        int resultNumerator = numerator * otherFraction.denominator - otherFraction.numerator * denominator;
+        Fraction result = new Fraction(resultNumerator, commonDenominator);
+        return result;
+    }
+
+
+    public Fraction Multiply(Fraction otherFraction)
+    {
+        Fraction result = new Fraction(numerator * otherFraction.numerator, denominator * otherFraction.denominator);
+        return result;
+    }
+
+    public Fraction Divide(Fraction otherFraction)
+    {
+        Fraction result = new Fraction(numerator * otherFraction.denominator, denominator * otherFraction.numerator);
+        return result;
+    }
+
+    public Fraction Sum(int number)
+    {
+        Fraction otherFraction = new Fraction(number, 1);
+        Fraction result = Sum(otherFraction);
+        return result;
+    }
+
+    public Fraction Difference(int number)
+    {
+        Fraction otherFraction = new Fraction(number, 1);
+        Fraction result = Difference(otherFraction);
+        return result;
+    }
+
+    public Fraction Multiply(int number)
+    {
+        Fraction otherFraction = new Fraction(number, 1);
+        Fraction result = Multiply(otherFraction);
+        return result;
+    }
+
+    public Fraction Divide(int number)
+    {
+        Fraction otherFraction = new Fraction(number, 1);
+        Fraction result = Divide(otherFraction);
+        return result;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Main
+﻿public class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-
-        }
+        Fraction fraction1 = new Fraction(1, 2);
+        fraction1.Print();
+        Fraction result = fraction1.Sum(1);
+        result.Print(); // 3/2
+        result = fraction1.Difference(1);
+        result.Print(); // -1/2
+        result = fraction1.Multiply(2);
+        result.Print(); // 2/2
+        result = fraction1.Divide(2);
+        result.Print(); // 1/4
     }
 }
