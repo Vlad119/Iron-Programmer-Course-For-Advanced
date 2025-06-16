6fdb0b714e7a4e8c36bfbbd1e8f9f219adc7c6b5
Vlad119 <unstarkble12@gmail.com>
2025-06-16 21:10:45 +0300

    Решил задачу 9_3_2

---
+++ Main/Main/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Item
+{
+    public string name;
+    public int weight;
+    public string specialAbility;
+
+    public Item() : this("", 0, "")
+    { }
+
+    public Item(string name, int weight) : this(name, weight, "")
+    { }
+
+    public Item(string name, int weight, string specialAbility)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.specialAbility = specialAbility;
+    }
+
+    public void ShowItem()
+    {
+        Console.WriteLine($"{name}:{weight}({specialAbility})");
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Item item1 = new Item();
+            Item item2 = new Item("Меч", 10);
+            Item item3 = new Item("Лук", 5, "Возгорание");
+            Item item4 = new Item("Щит", 15, "Дополнительный урон");
+            item1.ShowItem();
+            item2.ShowItem();
+            item3.ShowItem();
+            item4.ShowItem();
         }
     }
 }
