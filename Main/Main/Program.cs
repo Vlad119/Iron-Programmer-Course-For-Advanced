da6b4896be23657d2d5647cfa8bdd33266a89b86
Vlad119 <unstarkble12@gmail.com>
2025-06-16 21:22:29 +0300

    Решил задачу 9_3_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,12 +4,34 @@ namespace Main
 {
     internal class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Character character = new Character();
-            character.equipment.items[0].ShowItem();
-            character.equipment.items[1].ShowItem();
-            Console.WriteLine($"Общий вес: {character.equipment.GetWeightOfEquipment()}");
+            Character pers = new Character();
+            string[] itemsDescriptions = new string[] { "Щит 4", "Сабля 3", "Посох 4" };
+            Secret(pers, itemsDescriptions);
+        }
+
+        public static void Secret(Character pers, string[] itemsDescriptions)
+        {
+            Item[] items = new Item[itemsDescriptions.Length];
+            int totalWeight = 0;
+            for (int i = 0; i < itemsDescriptions.Length; i++)
+            {
+                string[] split = itemsDescriptions[i].Split();
+                string name = split[0];
+                int weight = Convert.ToInt32(split[1]);
+                totalWeight += weight;
+                items[i] = new Item(name, weight);
+            }
+            if (totalWeight > 20)
+            {
+                Console.WriteLine("Слишком тяжёлое снаряжение");
+            }
+            else
+            {
+                Console.WriteLine("Персонаж забрал снаряжение");
+                pers.equipment.items = items;
+            }
         }
     }
 }
