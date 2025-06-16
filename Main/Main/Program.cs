48ac01f27db386c653adcc81a935290d8accbbec
Vlad119 <unstarkble12@gmail.com>
2025-06-16 22:05:20 +0300

    Решил задачу 11_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Student student1 = new Student("Иосиф", 16);
+            Console.WriteLine(student1.GetAge());
+            student1.SetAge(25);
+            Console.WriteLine(student1.GetAge());
+            student1.SetAge(20);
+            student1.SetAge(15);
         }
     }
 }
---
+++ Main/Main/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Student
+{
+    private string name;
+    private int age;
+
+    public Student(string name, int age)
+    {
+        SetName(name);
+        SetAge(age);
+    }
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string value)
+    {
+        if (value.Length <= 1)
+        {
+            throw new Exception("Имя должно состоять как минимум из 2 букв.");
+        }
+
+        foreach (char c in value)
+        {
+            if (!char.IsLetter(c))
+            {
+                throw new Exception("Имя должно состоять только из букв.");
+            }
+        }
+        name = value;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public void SetAge(int value)
+    {
+        if (value < 16)
+        {
+            throw new Exception("Возраст должен быть не меньше 16 лет.");
+        }
+        if (value < age)
+        {
+            throw new Exception("Новый возраст не может быть меньше предыдущего.");
+        }
+        age = value;
+    }
+}
