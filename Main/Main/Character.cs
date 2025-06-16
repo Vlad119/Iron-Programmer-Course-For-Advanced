55a6a80170d8a9e1e6829109dc218c11d68ebbb2
Vlad119 <unstarkble12@gmail.com>
2025-06-16 21:18:21 +0300

    Решил задачу 9_3_4

---
+++ Main/Main/Character.cs
@@ -0,0 +1,12 @@
+﻿class Character
+{
+    public Equipment equipment;
+
+    public Character()
+    {
+        Item sword = new Item("Меч", 10);
+        Item bow = new Item("Лук", 5);
+        Item[] items = { sword, bow };
+        equipment = new Equipment(items);
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -6,13 +6,10 @@ namespace Main
     {
         public static void Main()
         {
-            Item item1 = new Item();
-            Item item2 = new Item("Меч", 10);
-            Item item3 = new Item("Лук", 5, "Возгорание");
-            Item item4 = new Item("Щит", 15, "Дополнительный урон");
-            Item[] items = { item1, item2, item3, item4 };
-            Equipment equipment = new Equipment(items);
-            Console.WriteLine($"Вес всех предметов: {equipment.GetWeightOfEquipment()}");
+            Character character = new Character();
+            character.equipment.items[0].ShowItem();
+            character.equipment.items[1].ShowItem();
+            Console.WriteLine($"Общий вес: {character.equipment.GetWeightOfEquipment()}");
         }
     }
 }
