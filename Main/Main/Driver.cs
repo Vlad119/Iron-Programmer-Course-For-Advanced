c33adc7d9b7dfd01f6a18e95b8767e3b96f8df3a
Vlad119 <unstarkble12@gmail.com>
2025-06-17 15:45:24 +0300

    Решил задачу 11_4_2

---
+++ Main/Main/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Driver
+{
+    public readonly Guid Id;
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public Driver(string name, string lastname, int age)
+    {
+        Id = Guid.NewGuid();
+        FirstName = name;
+        LastName = lastname;
+        Age = age;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Driver driver = new Driver("Иван", "Иванов", 34);
+            Console.WriteLine($"Новый водитель: {driver.FirstName} {driver.LastName}, возраст: {driver.Age}");
+            driver.FirstName = "Алексей";
+            driver.LastName = "Алексеев";
+            driver.Age = 30;
+            Console.WriteLine($"Новый водитель: {driver.FirstName} {driver.LastName}, возраст: {driver.Age}");
         }
     }
 }
