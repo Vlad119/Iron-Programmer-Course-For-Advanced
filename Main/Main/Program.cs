e5019ff2a81a86b0cf56b7e4b002d33dce07295d
Vlad119 <unstarkble12@gmail.com>
2025-06-17 16:11:29 +0300

    Решил задачу 11_4_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,25 +4,43 @@ public class Program
 {
     public static void Main()
     {
-        Driver driverA = new Driver("Иван", "Иванов", 34);
-        Driver driverC = new Driver("Пётр", "Петров", 25);
-        Car carA = new Car("A123MP", "Красный", driverA);
-        Car carB = new Car("B456XC", "Синий", null);
-        Car carC = new Car("K789BA", "Белый", driverC);
-        CarsDrivers(new Car[] { carA, carB, carC });
+        Car carA = new Car("O123MP", "Красный", null);
+        Car carB = new Car("M456XC", "Синий", null);
+        Car carC = new Car("K789BA", "Белый", null);
+        Car[] cars = new Car[] { carA, carB, carC };
+        ChangeColor(cars);
+        for (int i = 0; i < cars.Length; ++i)
+        {
+            Console.WriteLine($"{cars[i].Color}");
+        }
     }
 
-    public static void CarsDrivers(Car[] cars)
+    public static void ChangeColor(Car[] cars)
     {
         foreach (var car in cars)
         {
-            if (car.Driver != null)
+            if (car.Number.Length > 0)
             {
-                Console.WriteLine($"{car.Number}:{car.Driver.LastName}");
+                char firstLetter = car.Number[0];
+                switch (firstLetter)
+                {
+                    case 'O':
+                        car.Color = "Оранжевый";
+                        break;
+                    case 'C':
+                        car.Color = "Синий";
+                        break;
+                    case 'K':
+                        car.Color = "Красный";
+                        break;
+                    default:
+                        car.Color = "Белый";
+                        break;
+                }
             }
             else
             {
-                Console.WriteLine($"{car.Number}:Нет водителя");
+                car.Color = "Белый";
             }
         }
     }
