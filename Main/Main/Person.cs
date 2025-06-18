5a29350bff44ca421a7dc9b0ff04affe3c60f310
Vlad119 <unstarkble12@gmail.com>
2025-06-18 23:13:10 +0300

    Решил задачу 11_4_8

---
+++ Main/Main/Person.cs
@@ -0,0 +1,16 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public readonly Person Mom;
+    public readonly Person Dad;
+
+    public Person(string name, int age, Person mom, Person dad)
+    {
+        Name = name;
+        Age = age;
+        Mom = mom;
+        Dad = dad;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            RegisterBaby("Миша", new Person("Светлана", 23, null, null), new Person("Алексей", 24, null, null));
+            if (typeof(Person).GetField("Mom").IsInitOnly && typeof(Person).GetField("Dad").IsInitOnly)
+            {
+                Console.WriteLine("Ок");
+            }
+        }
+        public static void RegisterBaby(string name, Person mom, Person dad)
+        {
+            Person baby = new Person(name, 0, mom, dad);
+            if (typeof(Person).GetField("Mom").IsInitOnly && typeof(Person).GetField("Dad").IsInitOnly)
+            {
+                Console.WriteLine("Ок");
+            }
         }
     }
 }
