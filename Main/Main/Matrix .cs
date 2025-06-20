e660d1d4ffca987e330c779430dd0a7a6e7c1c28
Vlad119 <unstarkble12@gmail.com>
2025-06-20 11:10:59 +0300

    Решил задачу 11_5_4

---
+++ Main/Main/Matrix .cs
@@ -0,0 +1,19 @@
+﻿public class Matrix
+{
+    private int[,] numbers;
+    public Matrix()
+    {
+        numbers = new int[,]
+        {
+            { 1, 2, 4 },
+            { 2, 3, 6 },
+            { 3, 4, 8 }
+        };
+    }
+
+    public int this[int row, int col]
+    {
+        get { return numbers[row, col]; }
+        set { numbers[row, col] = value; }
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Matrix matrix = new Matrix();
+            Console.WriteLine(matrix[0, 1]);
+            matrix[0, 1] = 32;
+            Console.WriteLine(matrix[0, 1]);
         }
     }
 }
