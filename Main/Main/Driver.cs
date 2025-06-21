96e3d995768c5935101fd6aaabc4ad8e3986b314
Vlad119 <unstarkble12@gmail.com>
2025-06-21 15:13:13 +0300

    Решил задачу 12_1_2

---
+++ Main/Main/Driver.cs
@@ -0,0 +1,11 @@
+﻿public class Driver : User
+{
+    public char Category { get; set; }
+
+    public Driver (string name, int age, char cat)
+    {
+        Name = name;
+        Age = age;
+        Category = cat;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Driver d1 = new Driver("Василий", 35, 'B');
+            Driver d2 = new Driver("Виктор", 23, 'C');
+            Console.WriteLine(d1.Age + d2.Age);
         }
     }
 }
