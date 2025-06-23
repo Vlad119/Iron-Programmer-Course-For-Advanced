e09738dda6ebffe427201e7a7dc0a8d7d0fd71cf
Vlad119 <unstarkble12@gmail.com>
2025-06-23 11:50:05 +0300

    Решил задачу 12_2_3

---
+++ Main/Main/Figure.cs
@@ -0,0 +1,18 @@
+﻿public class Figure
+{
+    public int Layer { get; set; }
+
+    public string EdgeColor { get; set; } 
+
+    public double EdgeThickness { get; set; }
+
+    public string FillColor { get; set; }
+
+    public Figure()
+    {
+        Layer = 0;
+        EdgeColor = "чёрный";
+        EdgeThickness = 1;
+        FillColor = "белый";
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Figure figure = new Figure();
+            figure.Layer = 1;
+            Console.WriteLine($"Слой:{figure.Layer}, Цвет границ:{figure.EdgeColor}, Толщина границ:{figure.EdgeThickness}, Цвет заливки:{figure.FillColor}");
+            figure.EdgeThickness = 3.4;
+            figure.EdgeColor = "красный";
+            figure.FillColor = "зелёный";
+            Console.WriteLine($"Слой:{figure.Layer}, Цвет границ:{figure.EdgeColor}, Толщина границ: {figure.EdgeThickness}, Цвет заливки:{figure.FillColor}");
         }
     }
 }
