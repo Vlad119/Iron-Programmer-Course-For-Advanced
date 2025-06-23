73b85f9454ed884952151fa9d48a180d0c9793bc
Vlad119 <unstarkble12@gmail.com>
2025-06-23 12:04:28 +0300

    Решил задачу 12_2_5

---
+++ Main/Main/Circle.cs
@@ -0,0 +1,12 @@
+﻿public sealed class Circle : Figure
+{
+    public double Radius { get; set; }
+
+    public Point Center { get; set; }
+
+    public Circle()
+    {
+        Radius = 0;
+        Center = new Point(); 
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -6,10 +6,19 @@ namespace Main
     {
         public static void Main()
         {
-            Point point = new Point();
-            point.X_Position = 2;
-            point.Y_Position = 3.4;
-            Console.WriteLine($"X:{point.X_Position} Y:{point.Y_Position}");
+            Circle circle = new Circle();
+            circle.Radius = 10;
+            circle.Center = new Point()
+            {
+                X_Position = 3.6,
+                Y_Position = -2.5
+            };
+            circle.Layer = 3;
+            circle.EdgeThickness = 3.4;
+            circle.EdgeColor = "синий";
+            circle.FillColor = "зелёный";
+            Console.WriteLine($"Радиус:{circle.Radius}, X:{circle.Center.X_Position} Y:{circle.Center.Y_Position}");
+            Console.WriteLine($"Слой:{circle.Layer}, Цвет границ:{circle.EdgeColor}, Толщина границ:{circle.EdgeThickness}, Цвет заливки:{circle.FillColor}");
         }
     }
 }
