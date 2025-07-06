0eb4a2f34a0243491d663868ae3ac27c5f4374df
Vlad119 <unstarkble12@gmail.com>
2025-07-06 22:17:56 +0300

    Решил задачу 13_3_2

---
+++ Main/Main/Player.cs
@@ -0,0 +1,23 @@
+﻿
+internal class Player
+{
+    public string Name { get; }
+    public int Number { get; }
+    public Player(string name, int number)
+    {
+        Name = name;
+        Number = number;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        if (GetType() != obj.GetType()) return false;
+        Player other = (Player)obj;
+        return Name.Equals(other.Name) && Number.Equals(other.Number);
+    }
+
+    public override int GetHashCode()
+    {
+        return ToString().GetHashCode();
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Player player1 = new Player("Sergio Ramos", 4);
+            Player player2 = new Player("Sergio Ramos", 4);
+            Console.WriteLine(player1.Equals(player2));
         }
     }
 }
