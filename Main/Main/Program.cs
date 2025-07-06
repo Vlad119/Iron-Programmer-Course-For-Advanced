bc52e523978dcd6b4f4dceab3e4b034108162801
Vlad119 <unstarkble12@gmail.com>
2025-07-06 22:10:10 +0300

    Пример переопределения методов Equals и GetHashCode

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            User user1 = new User() { Name = "Josef" };
+            User user2 = new User() { Name = "Jo" };
+            Console.WriteLine(user1.Equals(user2));
+            Console.WriteLine(user2.Equals(user1));
+            Console.WriteLine(user1.Equals(user1));
+            Console.WriteLine(user2.Equals(user2));
+            Console.WriteLine(user1.GetHashCode() == user2.GetHashCode());
         }
+    }
+}
+
+public class User
+{
+    public string Name { get; set; }
+    public override string ToString()
+    {
+        return Name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        if (GetType() != obj.GetType()) return false;
+        User other = (User)obj;
+        return Name.Equals(other.Name);
+    }
+
+    public override int GetHashCode()
+    {
+        return ToString().GetHashCode();
     }
 }
