119da4e4ca98a6e12985e1cfddad97c820ab75b9
Vlad119 <unstarkble12@gmail.com>
2025-07-08 20:35:19 +0300

    Решил задачу 14_1_2

---
+++ Main/Main/GeometricArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class GeometricArea
+{
+    public double CalculateArea(double r)
+    {
+        return Math.PI * Math.Pow(r, 2);
+    }
+
+    public double CalculateArea(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double CalculateArea(double a, double b, double c)
+    {
+        var p = (a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
+
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            GeometricArea circle = new GeometricArea();
+            Console.WriteLine(circle.CalculateArea(3));
+            GeometricArea rectangle = new GeometricArea();
+            Console.WriteLine(rectangle.CalculateArea(3, 4));
+            GeometricArea triangle = new GeometricArea();
+            Console.WriteLine(triangle.CalculateArea(3, 4, 5));
         }
     }
 }
