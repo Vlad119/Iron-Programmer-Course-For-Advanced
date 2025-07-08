8527778694e7db1f099d7616c25864806879ee57
Vlad119 <unstarkble12@gmail.com>
2025-07-08 20:27:37 +0300

    Решил задачу 14_1_1

---
+++ Main/Main/GeometricPerimeter.cs
@@ -0,0 +1,17 @@
+﻿public class GeometricPerimeter
+{
+    public int CalculatePerimeter(int a)
+    {
+        return a * 4;
+    }
+
+    public int CalculatePerimeter(int a, int b)
+    {
+        return (a + b) * 2;
+    }
+
+    public int CalculatePerimeter(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
+internal class Program
+{
+    public static void Main()
+    {
+        GeometricPerimeter p = new GeometricPerimeter();
+        Console.WriteLine(p.CalculatePerimeter(2));
+        Console.WriteLine(p.CalculatePerimeter(3, 4));
+        Console.WriteLine(p.CalculatePerimeter(7, 3, 6));
     }
 }
+
