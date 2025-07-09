5430fb972855986a219fda046fec38efd46b25a9
Vlad119 <unstarkble12@gmail.com>
2025-07-09 22:34:36 +0300

    Решил задачу 14_2_5

---
+++ Main/Main/Fraction.cs
@@ -0,0 +1,22 @@
+﻿public class Fraction
+{
+    public int Numerator { get; set; }
+    public int Denominator { get; set; }
+
+    public Fraction(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    public override string ToString()
+    {
+        return $"{Numerator}/{Denominator}";
+    }
+
+    public static string operator *(Fraction f1, Fraction f2)
+    {
+        return $"{f1.Numerator * f2.Numerator}/{f1.Denominator * f2.Denominator}";
+    }
+
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 3);
+            Console.WriteLine(f1 * f2);
         }
     }
 }
