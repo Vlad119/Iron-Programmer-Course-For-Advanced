53e504543a517c2837b0202b0c75a05849b9865b
Vlad119 <unstarkble12@gmail.com>
2025-07-09 21:40:37 +0300

    Решил 14_2_3

---
+++ Main/Main/Integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Integer
+{
+    private int value;
+    public Integer(int n)
+    {
+        value = n;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"value: {value}");
+    }
+
+    public static int operator *(Integer integer, int number)
+    {
+        return integer.value * number;
+    }
+
+    public static double operator *(Integer integer, double number)
+    {
+        return integer.value * number;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
+            Integer integer1 = new Integer(15);
+            Integer integer2 = new Integer(8);
+            Console.WriteLine(integer1 * 3);
+            Console.WriteLine(integer2 * 2.5);
         }
     }
 }
