b06f27c5f88705fd8905bf8114270935d76ab2a3
Vlad119 <unstarkble12@gmail.com>
2025-07-10 21:56:52 +0300

    Решил задачу 14_2_9

---
+++ Main/Main/Post.cs
@@ -0,0 +1,17 @@
+﻿public class Post
+{
+    public int Id { get; set; }
+    public int Likes { get; set; }
+    public int Comments { get; set; }
+    public int Views { get; set; }
+
+    public static bool operator >(Post p1, Post p2)
+    {
+        return (double) (p1.Likes + p1.Comments * 5) / p1.Views > (double)(p2.Likes + p2.Comments * 5) / p2.Views;
+    }
+
+    public static bool operator <(Post p1, Post p2)
+    {
+        return (double)(p1.Likes + p1.Comments * 5) / p1.Views < (double)(p2.Likes + p2.Comments * 5) / p2.Views;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Main
+﻿using System;
+using System.Collections.Generic;
+
+namespace Main
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            List<Post> posts = new List<Post>()
+    {
+        new Post() { Id = 1, Likes = 240, Comments = 120, Views = 300},
+        new Post() { Id = 2, Likes = 210, Comments = 60, Views = 300},
+        new Post() { Id = 3, Likes = 150, Comments = 45, Views = 300}
+    };
 
+            Post newPost = new Post() { Id = 4, Likes = 70, Comments = 20, Views = 100 };
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine(newPost < post);
+                Console.WriteLine(newPost > post);
+            }
         }
     }
 }
