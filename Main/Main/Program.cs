40211b007b772dd185dbc7d980dc715f59b8cb75
Vlad119 <unstarkble12@gmail.com>
2025-07-12 10:54:53 +0300

    Решил задачу 14_3_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            int age = 25;
+            User user = (User)age;
+            Console.WriteLine(user.Name + " " + user.Age);
+            User user2 = new User("Josef", 26);
+            int age2 = user2;
+            Console.WriteLine(age2);
         }
     }
 }
---
+++ Main/Main/User.cs
@@ -0,0 +1,21 @@
+﻿public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public User(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public static explicit operator User(int age)
+    {
+        return new User("Неизвестно", age);
+    }
+
+    public static implicit operator int(User user)
+    {
+        return user.Age;
+    }
+}
