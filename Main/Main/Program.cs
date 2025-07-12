6f0c58695117164b1420b21502ca9c08179e1564
Vlad119 <unstarkble12@gmail.com>
2025-07-12 10:39:36 +0300

    Решил задачу 14_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Student stud1 = new Student(1, "Сергей", "Березов");
+            string s = (string)stud1;
+            Console.WriteLine(s);
         }
     }
 }
---
+++ Main/Main/Student.cs
@@ -0,0 +1,18 @@
+﻿public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Student(int id, string name, string surname)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+    }
+
+    public static explicit operator string(Student student)
+    {
+        return $"{student.Name} {student.Surname}";
+    }
+}
