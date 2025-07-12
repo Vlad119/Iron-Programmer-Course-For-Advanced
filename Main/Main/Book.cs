635592ac716987430b1e25bc892be16dc48d0b98
Vlad119 <unstarkble12@gmail.com>
2025-07-12 11:14:47 +0300

    Решил задачу 14_3_4

---
+++ Main/Main/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace Main
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+
+        public Book(int id, string title, Author author)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+        }
+
+        public static explicit operator int(Book book)
+        {
+            return book.Id;
+        }
+
+        public static implicit operator string(Book book)
+        {
+            return book.Title;
+        }
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Book book = new Book(1, "Мёртвые души", new Author() { Name = "Н.В. Гоголь" });
+            Console.WriteLine($"{(int)book} {(string)book}");
         }
     }
 }
