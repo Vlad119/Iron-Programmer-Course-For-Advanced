39a307a7dd2ffba6a62cb851099fe785d265c4b1
Vlad119 <unstarkble12@gmail.com>
2025-07-13 14:41:11 +0300

    Решил задачу 16_1_5

---
+++ Main/Main/Person.cs
@@ -0,0 +1,20 @@
+﻿public class Person
+{
+    public static int Age { get; set; }
+    public static int LegalAge { get; set; } = 18;
+
+    public Person(int age)
+    {
+        Age = age;
+    }
+
+    public static int IncreaseLegalAge(int age)
+    {
+        return LegalAge++;
+    }
+
+    public bool IsAdult()
+    {
+        return Age >= LegalAge;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person pers = new Person(18);
+            if (pers.IsAdult())
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
 
+            Person.IncreaseLegalAge(1);
+            if (pers.IsAdult())
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
         }
     }
 }
