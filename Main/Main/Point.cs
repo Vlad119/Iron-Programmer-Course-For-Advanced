a8b00ffa8e1adcd5bd9bc61768b51ba70e19c050
Vlad119 <unstarkble12@gmail.com>
2025-07-13 13:19:34 +0300

    Решил задачу 16_1_3

---
+++ Main/Main/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Main
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public static int Counter { get; set; } = 0;
+
+        public Point(int x, int y)
+        {
+            Counter++;
+            X = x;
+            Y = y;
+        }
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Point point1 = new Point(1, 1);
+            Console.WriteLine(Point.Counter);
 
+            Point point2 = new Point(2, 3);
+            Console.WriteLine(Point.Counter);
         }
     }
 }
