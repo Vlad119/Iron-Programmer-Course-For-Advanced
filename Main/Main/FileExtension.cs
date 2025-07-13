bc32df24b34cfa159d60d945726e696ce26be16f
Vlad119 <unstarkble12@gmail.com>
2025-07-13 17:31:06 +0300

    Решил задачу 16_3_4

---
+++ Main/Main/FileExtension.cs
@@ -0,0 +1,17 @@
+﻿using static Program;
+
+public static class FileExtension
+{
+    internal static string GetFileExtension(this FileFormat fileFormat)
+    {
+        switch(fileFormat)
+        {
+            case FileFormat.PlainText: { return "txt";}
+            case FileFormat.OfficeWord: { return "docx"; }
+            case FileFormat.Markdown: { return "md"; }
+            case FileFormat.Json: { return "json"; }
+            case FileFormat.XML: { return "xml"; }
+        }
+        return string.Empty;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Main
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
+internal class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(FileFormat.PlainText.GetFileExtension());
+        Console.WriteLine(FileFormat.OfficeWord.GetFileExtension());
+        Console.WriteLine(FileFormat.Markdown.GetFileExtension());
+        Console.WriteLine(FileFormat.Json.GetFileExtension());
+        Console.WriteLine(FileFormat.XML.GetFileExtension());
+    }
+
+    public enum FileFormat
+    {
+        PlainText,
+        OfficeWord,
+        Markdown,
+        Json,
+        XML
     }
 }
+
