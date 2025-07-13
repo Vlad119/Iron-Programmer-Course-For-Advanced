c559bed8b7328dad8f61de33cacf424a826af271
Vlad119 <unstarkble12@gmail.com>
2025-07-13 17:42:23 +0300

    второй вариант

--- Main/Main/ProductDBExtension.cs
+++ Main/Main/ProductDBExtension.cs
@@ -2,9 +2,10 @@
 {
     public static ProductViewModel ToProductViewModel(this ProductDb product)
     {
-        var PVM = new ProductViewModel();
-        PVM.Name = product.Name;
-        PVM.Cost = product.Cost;
-        return PVM;
+        return new ProductViewModel
+        {
+            Name = product.Name,
+            Cost = product.Cost
+        };
     }
 }
