b0209eb340c7d5f3671e75591ec027e8e0d80451
Vlad119 <unstarkble12@gmail.com>
2025-07-13 11:40:47 +0300

    Решил задачу 15_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Main
+﻿using System;
+
+namespace Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Random rnd = new Random();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int random = rnd.Next(100);
+                Console.WriteLine(random > 50 ? "Гусь": "Утка");
+            }
         }
     }
 }
