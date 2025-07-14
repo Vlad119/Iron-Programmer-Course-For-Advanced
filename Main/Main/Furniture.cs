66b8d86a37d472154b7da63bc976da47c5fdd0be
Vlad119 <unstarkble12@gmail.com>
2025-07-14 14:53:52 +0300

    Решил задачу 17_2_2

---
+++ Main/Main/Furniture.cs
@@ -0,0 +1,10 @@
+﻿public class Furniture
+{
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public int Price { get; set; }
+    public string Type { get; set; }
+    public bool InStock { get; set; }
+}
