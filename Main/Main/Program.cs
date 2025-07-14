83eb15ecec64b5ced816827cd3ad19ed706ab68f
Vlad119 <unstarkble12@gmail.com>
2025-07-14 14:29:27 +0300

    Решил задачу 17_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            UserProfile usProf = new UserProfile("Алина") { Gender = "Жен" };
+            usProf.DisplayUserInfo();
         }
     }
 }
---
+++ Main/Main/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class UserProfile
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Country { get; set; }
+
+    public UserProfile(string name)
+    {
+        Name = name;
+    }
+
+    public void DisplayUserInfo()
+    {
+        Console.WriteLine($"Имя: {Name??"Имя не указано"}"); 
+        Console.WriteLine($"Пол: {Gender??"Пол не указан"}"); 
+        Console.WriteLine($"Страна: {Country??"Страна не указана"}"); 
+    }
+}
+
