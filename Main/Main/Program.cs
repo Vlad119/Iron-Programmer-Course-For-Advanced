4f61c75919deeac0d4532a50cb3bfefd5a2364a8
Vlad119 <unstarkble12@gmail.com>
2025-07-20 12:53:07 +0300

    Решил задачу 19_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        if (SafeDivide(10, 2, out decimal result)) Console.WriteLine(result);
+        else Console.WriteLine("Деление невозможно");
+    }
 
+    static bool SafeDivide(decimal a, decimal b, out decimal result)
+    {
+        result = 0M;
+        if (b != 0)
+        {
+            result = a / b;
+            return true;
+        }
+        return false;
     }
 }
