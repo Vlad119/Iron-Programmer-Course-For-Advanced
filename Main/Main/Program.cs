0944487b8896b24183cc2ce5ca315004c4413c23
Vlad119 <unstarkble12@gmail.com>
2025-07-20 15:18:26 +0300

    Решил задачу 20_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        Reverse(numbers);
+        Console.WriteLine(string.Join(", ", numbers));
 
+        List<string> names = new List<string> { "Josef", "Vadim", "Ivan" };
+        Reverse(names);
+        Console.WriteLine(string.Join(", ", names));
+    }
+
+    static void Reverse<T>(List<T> list)
+    {
+        list.Reverse();
     }
 }
