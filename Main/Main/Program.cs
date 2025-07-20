f699b9a493bedfb3797bc138e899810af01827c5
Vlad119 <unstarkble12@gmail.com>
2025-07-20 16:57:57 +0300

    Решил задачу 21_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
+    delegate bool Predicate(int a);
     static void Main(string[] args)
     {
+        var x = 25;
+        Predicate predicate = IsAdult;
+        var result = predicate(x);
+        Console.WriteLine(result);
+    }
 
+    static bool IsAdult(int age)
+    {
+        return age >= 18;
     }
 }
