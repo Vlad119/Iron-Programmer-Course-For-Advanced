0a7f310ba0b521251731fd06bbc71907c1d5d3fc
Vlad119 <unstarkble12@gmail.com>
2025-07-20 15:01:59 +0300

    Решил задачу 20_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
 
+internal class Program
+{
+    static void Main()
+    {
+        string[] names = { "Josef", "Vadim", "Ivan" };
+        PrintArray(names);
+    }
+
+    static void PrintArray<T>(T[] array)
+    {
+        foreach (T item in array)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
