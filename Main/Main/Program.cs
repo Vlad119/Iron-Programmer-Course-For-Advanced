f97d1d19cd7fa09b6a5889675d2bc2d77492837a
Vlad119 <unstarkble12@gmail.com>
2025-07-20 16:48:33 +0300

    Пример работы делегата

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,40 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Diagnostics;
+using System.Text;
 
+namespace CSharpEssentials
+{
+    class Program
+    {
+        delegate void MeasureMethod(int repeatCount);
+        static void Main()
+        {
+            Measure(StringMeasure);
+            Measure(StringBuilderMeasure);
+        }
+        private static void Measure(MeasureMethod measureMethod)
+        {
+            int repeatCount = 10000;
+            Stopwatch time = Stopwatch.StartNew();
+            measureMethod(repeatCount);
+            time.Stop();
+            Console.WriteLine(time.Elapsed.TotalMilliseconds);
+        }
+        private static void StringBuilderMeasure(int repeatCount)
+        {
+            StringBuilder stringBuilderResult = new StringBuilder("Test");
+            for (int i = 0; i < repeatCount; i++)
+            {
+                stringBuilderResult.Append("{i}");
+            }
+        }
+        private static void StringMeasure(int repeatCount)
+        {
+            string stringResult = "Test";
+            for (int i = 0; i < repeatCount; i++)
+            {
+                stringResult += "{i}";
+            }
+        }
     }
 }
