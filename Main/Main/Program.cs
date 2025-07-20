be38b1c15d9a6a253380f293f0ab8d30f7b55136
Vlad119 <unstarkble12@gmail.com>
2025-07-20 17:10:57 +0300

    Решил задачу 21_1_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
 
+namespace CSharpEssentials
+{
+    class Program
+    {
+        delegate bool Predicate(int year);
+        static void Main()
+        {
+            List<int> years = new List<int> { 13, 17, 18, 19, 20, 50 };
+            List<int> adults = GetAdults(years, IsAdult);
+            foreach (var adult in adults)
+            {
+                Console.WriteLine(adult);
+            }
+        }
+
+        private static bool IsAdult(int year)
+        {
+            return year >= 18;
+        }
+
+        private static List<int> GetAdults(List<int> years, Predicate predicate)
+        {
+            var adults = new List<int>();
+            foreach (var year in years)
+            {
+                if(predicate(year)) adults.Add(year);
+            }
+            return adults;
+        }
     }
 }
