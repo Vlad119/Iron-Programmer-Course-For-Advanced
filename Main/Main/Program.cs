e7522cf11970c7856b1baa90b63a783aa51c6f36
Vlad119 <unstarkble12@gmail.com>
2025-07-20 15:05:38 +0300

    Решил задачу 20_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
 
+internal class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 2, 4, 2 };
+        int occurrences = FindOccurrencesCount(numbers, 2);
+        Console.WriteLine(occurrences);
+
+        string[] names = { "Josef", "Vadim", "Josef", "Ivan" };
+        int occurrences2 = FindOccurrencesCount(names, "Josef");
+        Console.WriteLine(occurrences2);
     }
+
+    static int FindOccurrencesCount<T>(T[] array, T item)
+    {
+        int result = 0;
+        foreach(T item2 in array)
+        {
+            if(item2.Equals(item)) result++;
+        }
+        return result;
+    }
+
+
 }
