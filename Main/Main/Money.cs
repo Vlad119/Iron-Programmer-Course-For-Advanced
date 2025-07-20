6ae790dddc2ee5ed05fca70243fd56ceec1d7cc7
Vlad119 <unstarkble12@gmail.com>
2025-07-20 11:57:36 +0300

    Почти решил задачу 18_1_1

---
+++ Main/Main/Money.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public struct Money
+{
+    private int ruble;
+    private int kopeck;
+
+    public int Ruble
+    {
+        get => ruble;
+        set
+        {
+            if ((value < 0 && kopeck > 0) || (value > 0 && kopeck < 0))
+                throw new Exception();
+            ruble = value;
+        }
+    }
+
+    public int Kopeck
+    {
+        get => kopeck;
+        set
+        {
+            if (value < -99 || value > 99)
+                throw new Exception();
+            if ((value < 0 && ruble > 0) || (value > 0 && ruble < 0))
+                throw new Exception();
+            kopeck = value;
+        }
+    }
+
+    public Money(int ruble, int kopeck)
+    {
+        this.ruble = 0;
+        this.kopeck = 0;
+        Ruble = ruble;
+        Kopeck = kopeck;
+    }
+
+    public override string ToString()
+    {
+        string formattedRuble = (Ruble == 0 && Kopeck < 0) ? "-0" : Ruble.ToString();
+        Ruble = Convert.ToInt32(formattedRuble);
+        return $"{Ruble}, {Math.Abs(Kopeck):D2}";
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        //Money money1 = new Money(10, 50);
+        //Console.WriteLine(money1);
 
+        //money1.Ruble = 4;
+        //money1.Kopeck = 5;
+        //Console.WriteLine(money1);
+
+        //money1.Kopeck = 100;
+
+        //Money money2 = new Money(10, -50);
+
+        Money money3 = new Money(-10, -50);
+        Console.WriteLine(money3);
+
+
+        Money money4 = new Money(0, -20);
+        Console.WriteLine(money4);
     }
 }
