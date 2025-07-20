52d113b2748a2f06ca51cd9c8d9e07c95f592a3f
Vlad119 <unstarkble12@gmail.com>
2025-07-20 12:40:14 +0300

    Решил задачу 19_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        decimal senderBalance = 100;
+        decimal receiverBalance = 50;
+        decimal amount = 30;
+        bool isSuccess = Transfer(ref senderBalance, ref receiverBalance, amount);
+        Console.WriteLine($"Выполнение операции {isSuccess}. Отправитель: {senderBalance}, Получатель: {receiverBalance}");
+    }
 
+    static bool Transfer(ref decimal senderBalance, ref decimal receiverBalance, decimal amount)
+    {
+        if (senderBalance - amount >= 0) 
+        {
+            senderBalance -= amount;
+            receiverBalance += amount;
+            return true;
+        }
+        return false;
     }
 }
