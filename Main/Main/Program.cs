ba5b82f2158bb67591f2577f7d992b1612f750c4
Vlad119 <unstarkble12@gmail.com>
2025-07-21 12:38:51 +0300

    Решил задачу 21_1_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,64 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
 
+namespace CSharpEssentials
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {Age}";
+        }
+    }
+
+    class Program
+    {
+        delegate void Rule(User user);
+        private static void LongNameRule(User user)
+        {
+            if (user.Name.Length > 10)
+            {
+                user.Name = user.Name.Substring(0, 10);
+            }
+        }
+
+        private static void IncreaseAge(User user)
+        {
+            user.Age++;
+        }
+
+        private static List<User> TransformUsersByRule(List<User> users, Rule rule)
+        {
+            foreach (var user in users)
+            {
+                rule(user);
+            }
+            return users;
+        }
+
+        static void Main()
+        {
+            List<User> users = new List<User>
+            {
+                new User("namenamename", 25),
+                new User("1234567890", 34),
+                new User("123456", 14),
+            };
+            // сделать только ОДИН вызов метода TransformUsersByRule
+            Rule rule = LongNameRule;
+            rule += IncreaseAge;
+            List<User> transformedUsers = TransformUsersByRule(users, rule);
+            foreach (var user in transformedUsers)
+            {
+                Console.WriteLine(user);
+            }
+        }
     }
 }
