cc0c4dd5f25aa8fb041e7f99abe7db4610175688
Vlad119 <unstarkble12@gmail.com>
2025-07-24 15:51:53 +0300

    Решил задачу 21_2_4

---
+++ Main/Main/Product.cs
@@ -0,0 +1,13 @@
+﻿public class Product
+{
+    public string Name;
+    public decimal Price;
+    public string Category;
+
+    public Product(string name, decimal cost, string category)
+    {
+        Name = name;
+        Price = cost;
+        Category = category;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,47 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        List<Product> products = new List<Product>()
+    {
+        new Product("product1", 10, "furniture"),
+        new Product("product2", 100, "toy"),
+        new Product("product3", 1000, "toy"),
+        new Product("product4", 10000, "toy"),
+        new Product("product5", 100000, "furniture")
+    };
 
+        SelectAndChangePrice(products, "toy", 10, SelectCategory, IncreasePrice);
+        foreach (var item in products)
+        {
+            Console.WriteLine($"{item.Name}, категория: {item.Category}, цена: {(int)item.Price}");
+        }
     }
+
+    static void SelectAndChangePrice(List<Product> products, string category, int price, Func<Product, string, bool> func, Action<Product, int> action)
+    {
+        foreach (var product in products)
+        {
+            if (func(product, category))
+            {
+                action(product, price);
+            }
+        }
+    }
+
+    static void IncreasePrice(Product prod, int n)
+    {
+        decimal percent = prod.Price / 100 * n;
+        prod.Price += percent;
+    }
+
+    static bool SelectCategory(Product prod, string category)
+    {
+        return prod.Category.Equals(category);
+    }
+
+
 }
