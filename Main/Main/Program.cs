7f9955954bf91680aaeaf3aa1100b5aff5e1a665
Vlad119 <unstarkble12@gmail.com>
2025-07-24 15:00:35 +0300

    Решил задачу 21_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpEssentials
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main()
+        {
+            Print(new List<int> { 1, 2, 3, 4, 5 }, IsEven);
+        }
+
+        static void Print(List<int> items, Func<int, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+
+        // Создайте метод IsEven
+
+        static bool IsEven(int item)
+        {
+            return item % 2 == 0;
+        }
 
     }
 }
