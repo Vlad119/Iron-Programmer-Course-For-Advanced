0e819938658385367ac697d32574fe9970f81162
Vlad119 <unstarkble12@gmail.com>
2025-07-24 14:52:06 +0300

    Решил задачу 21_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main()
+    {
+        Print(new List<int> { 1, 2, 3, 4, 5 }, Twice);
+    }
+
+    static void Print(List<int> items, Action<int> action)
+    {
+        foreach (var item in items)
+        {
+            action(item);
+        }
+    }
+
+    // Создайте метод Twice
+
+    static void Twice(int item)
+    {
+        Console.Write(item * 2 + " ");
     }
 }
