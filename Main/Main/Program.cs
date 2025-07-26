97de5ed5f67ea37e2af79d4f5c288d8fd7d3dc67
Vlad119 <unstarkble12@gmail.com>
2025-07-26 09:45:14 +0300

    Решил задачу 21_2_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -7,11 +7,13 @@ namespace CSharpEssentials
     {
         static void Main()
         {
-            Func<int, bool> predicate = (item) =>
-            {
-                if (item * 2 % 10 == 6) return true;
-                return false;
-            };
+            //Func<int, bool> predicate = (item) =>
+            //{
+            //    if (item * 2 % 10 == 6) return true;
+            //    return false;
+            //};
+
+            Func<int, bool> predicate = item => 2 * item % 10 == 6;
 
             Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
         }
