0c6c711d14f03f58a0ea8f3748206094797ca9f8
Vlad119 <unstarkble12@gmail.com>
2025-07-26 09:20:10 +0300

    Решил задачу 21_2_5

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
 
+namespace CSharpEssentials
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, bool> predicate = (item) =>
+            {
+                if (Math.Pow(item, 2) % 10 == 9) return true;
+                return false;
+            };
+
+            Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
+        }
+
+        static void Print(List<int> items, Func<int, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
     }
 }
