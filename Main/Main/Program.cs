5a911abe52414bf2b0ffaaf878b0c2f8e7d581f9
Vlad119 <unstarkble12@gmail.com>
2025-07-26 09:43:42 +0300

    Решил задачу 21_2_7

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
 
+namespace Stepik
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> group = new List<Student> {
+            new Student("Паша", 45),
+            new Student("Гриша", 58),
+            new Student("Сергей", 63),
+            new Student("Олег", 87)
+            };
+
+            //Console.WriteLine("Надо сдавать зачет:");
+
+            //Вывод автоматов
+            //Console.WriteLine("Автоматы:");
+            //Show(group, x => x.Mark > 85);
+
+            //Вывод незачетов
+            //Console.WriteLine("Незачет:");
+            //Show(group, x => x.Mark < 51);
+
+            Console.WriteLine("Надо сдавать зачет:");
+            Show(group, student => student.Mark > 50 && student.Mark < 86);
+        }
+        // здесь напишите метод Show
+
+        static void Show(List<Student> students, Func<Student, bool> criteria)
+        {
+            foreach (var student in students)
+            {
+                if (criteria(student))
+                {
+                    Console.WriteLine($"{student.Name} - {student.Mark}");
+                }
+            }
+        }
     }
 }
---
+++ Main/Main/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Stepik
+{
+    public class Student
+    {
+        public readonly string Name;
+        public readonly int Mark;
+
+        public Student(string name, int mark)
+        {
+            Name = name;
+            Mark = mark;
+        }
+    }
+}
