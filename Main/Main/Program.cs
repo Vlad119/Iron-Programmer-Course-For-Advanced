66c49f949328492ba18f93e6ad8cfd5599a009d4
Vlad119 <unstarkble12@gmail.com>
2025-08-11 16:39:01 +0300

    Решил задачу 21_3_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 internal class Program
 {
     public static void Main()
     {
-        EventArgs arg = new UserRegisteredEventArgs("Test2@yandex.ru");
-        var info = typeof(UserRegisteredEventArgs).GetProperty("Email");
-        if (info.CanWrite == false)
-            Console.WriteLine("Ok");
-        else
-            throw new Exception("Свойство Email должно быть только для чтения");
+        User user = new User("Ivan Ivanov", "test@yandex.ru", "123321");
 
+        UserManager userManager = new UserManager();
+        userManager.Register(user);
+
+        var foundUser = userManager.Users.FirstOrDefault(x => x.Name.Equals(user.Name) && x.Email.Equals(user.Email) && x.Password.Equals(user.Password));
+        if (foundUser == null)
+            throw new Exception("Ошибка в добавлении пользователя");
+        else
+            Console.WriteLine("Ok");
     }
+
 }
--- Main/Main/UserManager.cs
+++ Main/Main/UserManager.cs
@@ -1,12 +1,15 @@
-﻿
+﻿using System;
 using System.Collections.Generic;
 
 public class UserManager
 {
     public List<User> Users { get; } = new List<User>();
 
+    public event EventHandler<UserRegisteredEventArgs> UserRegistered;
+
     public void Register(User user)
     {
         Users.Add(user);
+        UserRegistered?.Invoke(this, new UserRegisteredEventArgs(user.Email));
     }
 }
