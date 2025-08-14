f7cd170f66f0e24b43b00f089012980daf20ad31
Vlad119 <unstarkble12@gmail.com>
2025-08-14 15:04:44 +0300

    Решил задачу 22_1_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,38 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        IStudent student = new Student("Josef");
+        Console.WriteLine(student.Name);
+        student.ListeningLection();
+        student.DoHomework("123");
+        student.PrepareForExam();
+    }
+}
 
+public class Student : IStudent
+{
+    public Student(string value)
+    {
+         Name = value;
+    }
+
+    public string Name { get; set; }
+
+    public void DoHomework(string value)
+    {
+        Console.WriteLine("DoHomework");
+    }
+
+    public void ListeningLection()
+    {
+        Console.WriteLine("ListeningLection");
+    }
+
+    public void PrepareForExam()
+    {
+        Console.WriteLine("PrepareForExam");
     }
 }
