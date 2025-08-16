2b8bca5e881393e40b96edb5390fc9c43320e9ce
Vlad119 <unstarkble12@gmail.com>
2025-08-16 12:02:31 +0300

    Решил задачу 22_1_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
 
+internal class Program
+{
+    public static void Main()
+    {
+        var user = new User()
+        {
+            Id = 1,
+            Name = "Ivan",
+            Age = 25
+        }
+        ;
+        Console.WriteLine($"Id = {user.Id}, Name = {user.Name}, Age = {user.Age}");
     }
 }
