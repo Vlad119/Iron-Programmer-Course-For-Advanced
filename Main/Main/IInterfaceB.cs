b7c5ecb0a0b8bb404bd988a507995d4c02219b94
Vlad119 <unstarkble12@gmail.com>
2025-08-18 10:29:50 +0300

    Решил задачу 22_2_3

--- Main/Main/IInterfaceB.cs
+++ Main/Main/IInterfaceB.cs
@@ -1,4 +1,4 @@
-﻿public interface IInterfaceB : IInterfaceA
+﻿public interface IInterfaceB
 {
     string Prop3 { get; set; }
     void Method1(string param);
--- Main/Main/MyClass.cs
+++ Main/Main/MyClass.cs
@@ -1,8 +1,8 @@
-﻿public class MyClass : IInterfaceB
+﻿public class MyClass : IInterfaceA, IInterfaceB
 {
-    public string Prop3 { get ; set ; }
-    public bool Prop1 { get ; set ; }
-    public int Prop2 { get; set ; }
+    public bool Prop1 { get; set; }
+    public int Prop2 { get; set; }
+    public string Prop3 { get; set; }
 
     public void Method1(string param)
     { }
