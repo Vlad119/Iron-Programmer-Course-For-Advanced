d8c7849f9370af1a2f8e26ae594e6f20f8e9b982
Vlad119 <unstarkble12@gmail.com>
2025-08-18 10:47:39 +0300

    Решил задачу 22_3_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,48 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
 
+namespace CSharpEssentials
+{
+    public interface IInterfaceA
+    {
+        void Method1();
+    }
+
+    public interface IInterfaceB
+    {
+        void Method1();
+    }
+
+    public class MyClass : IInterfaceA, IInterfaceB
+    {
+        // добавьте недостающие члены
+        void IInterfaceA.Method1()
+        {
+            Console.WriteLine("IInterfaceA.Method1");
+        }          
+        
+        void IInterfaceB.Method1()
+        {
+            Console.WriteLine("IInterfaceB.Method1");
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("MyClass.Method1");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.Method1();
+
+            IInterfaceA a = myClass;
+            a.Method1();
+
+            IInterfaceB b = myClass;
+            b.Method1();
+        }
     }
 }
