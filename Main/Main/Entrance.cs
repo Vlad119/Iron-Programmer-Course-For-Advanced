7b46eb4a63595c33297a54e2a9c758399300c183
Vlad119 <unstarkble12@gmail.com>
2025-08-19 14:40:00 +0300

    Решил задачу 22_4_2

--- Main/Main/Entrance.cs
+++ Main/Main/Entrance.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 
-public class Entrance : IEnumerable
+public class Entrance : IEnumerable, IEnumerator
 {
     private readonly Flat[] flats;
-
+    private int currentIndex = -1;
+    public object Current => flats[currentIndex];
     public Entrance(Flat[] _flats) => flats = _flats;
-    public IEnumerator GetEnumerator() => new EntranceEnumerator(flats);
+
+    public IEnumerator GetEnumerator() => this;
+
+    public bool MoveNext()
+    {
+        currentIndex++;
+        if (currentIndex >= flats.Length)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void Reset() => currentIndex = -1;
 }
