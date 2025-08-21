0166cbe381290e8738fb1ca0cb1728623c33310f
Vlad119 <unstarkble12@gmail.com>
2025-08-21 19:12:20 +0300

    Решил задачу 22_4_6

--- Main/Main/Entrance.cs
+++ Main/Main/Entrance.cs
@@ -1,14 +1,24 @@
-﻿public class Entrance 
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Entrance : IEnumerable<Flat>, IEnumerator<Flat>
 {
     private readonly Flat[] flats;
     private int index = -1;
     public Entrance(Flat[] _flats) => flats = _flats;
-    public Entrance GetEnumerator() => this;
     public Flat Current => flats[index];
+    Flat IEnumerator<Flat>.Current => flats[index];
+
+    object IEnumerator.Current => Current;
+
+    IEnumerator IEnumerable.GetEnumerator() => this;
+    IEnumerator<Flat> IEnumerable<Flat>.GetEnumerator() => this;
+
     public bool MoveNext()
     {
         index++;
         return index < flats.Length;
     }
     public void Reset() => index = -1;
+    public void Dispose() { }
 }
