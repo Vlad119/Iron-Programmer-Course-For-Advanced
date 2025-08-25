146c88300eba316cf4752460830d9f26d2206996
Vlad119 <unstarkble12@gmail.com>
2025-08-25 15:12:43 +0300

    Решил задачу 23_1_3

---
+++ Main/Main/Gun.cs
@@ -0,0 +1,11 @@
+﻿public abstract class Gun
+{
+    public int BulletsCapacity { get; set; }
+    public int Damage { get; set; }
+    protected Gun(int bulletsCapacity, int damage)
+    {
+        BulletsCapacity = bulletsCapacity;
+        Damage = damage;
+    }
+    public abstract void Shoot();
+}
