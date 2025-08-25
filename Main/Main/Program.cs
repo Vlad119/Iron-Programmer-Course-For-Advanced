516e2e593676c3b5c93c2a7f42e3107b171f38d1
Vlad119 <unstarkble12@gmail.com>
2025-08-25 10:57:26 +0300

    Решил задачу 22_5_4

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Student[] students = 
+        {
+            new Student("Игорь Курчатов", 6, 4.8),
+            new Student("Сергей Королёв", 7, 4.0),
+            new Student("Юлий Харитон", 7, 4.5),
+            new Student("Николай Вавилов", 2, 4.8),
+            new Student("Михаил Калашников", 2, 4.3),
+            new Student("Андрей Сахаров", 11, 3.9),
+            new Student("Пётр Капица", 7, 4.5)
+        };
+        Array.Sort(students);
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
     }
 }
---
+++ Main/Main/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Student : IComparable<Student>
+{
+    public string FIO; // имя
+    public int Grade; // класс
+    public double Performance; // средний балл
+
+    public Student(string fio, int grade, double performance)
+    {
+        FIO = fio;
+        Grade = grade;
+        Performance = performance;
+    }
+
+    public int CompareTo(Student other)
+    {
+        int result = Grade.CompareTo(other.Grade);
+        if (result != 0) return result;
+        result = Performance.CompareTo(other.Performance);
+        if (result != 0) return result;
+        return FIO.CompareTo(other.FIO);
+    }
+
+    public override string ToString()
+    {
+        return $"Имя: {FIO}, Класс: {Grade}, Средний балл: {Performance}";
+    }
+
+}
