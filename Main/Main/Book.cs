f7c7b5b38434d4479497ba93dc6ec0a99e971be4
Vlad119 <unstarkble12@gmail.com>
2025-08-25 10:33:39 +0300

    Решил задачу 22_5_1

---
+++ Main/Main/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Book : IComparable<Book>
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int ReleaseYear { get; set; }
+
+    public int CompareTo(Book other) => ReleaseYear.CompareTo(other.ReleaseYear);
+    public override string ToString() => $"{Name}, {Author}, {ReleaseYear}";
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Book[] books =
+        {
+            new Book {Name = "Предел", Author = "Сергей Лукьяненко", ReleaseYear = 2021},
+            new Book {Name = "Порог", Author = "Сергей Лукьяненко", ReleaseYear = 2020},
+            new Book {Name = "Чистая архитектура", Author = "Мартин Роберт", ReleaseYear = 2018},
+            new Book {Name = "C# для профессионалов", Author = "Скит Джон", ReleaseYear = 2019}
+        };
+        Array.Sort(books);
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
     }
 }
