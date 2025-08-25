670535e218993bb3df9adfcd63a7b2c79d2326f4
Vlad119 <unstarkble12@gmail.com>
2025-08-25 16:42:18 +0300

    Решил задачу 24_1_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
+    public enum TrafficLight
+    {
+        Red, Yellow, Green
+    }
+
+
     static void Main(string[] args)
     {
-
+        Console.WriteLine((int)TrafficLight.Red);
+        Console.WriteLine((int)TrafficLight.Yellow);
+        Console.WriteLine((int)TrafficLight.Green);
     }
 }
