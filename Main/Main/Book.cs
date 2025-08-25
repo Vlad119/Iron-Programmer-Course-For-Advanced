109ba470af4948959670ccd622e8b6c2456bcf77
Vlad119 <unstarkble12@gmail.com>
2025-08-25 10:38:52 +0300

    Решил задачу 22_5_3

--- Main/Main/Book.cs
+++ Main/Main/Book.cs
@@ -6,6 +6,11 @@ public class Book : IComparable<Book>
     public string Author { get; set; }
     public int ReleaseYear { get; set; }
 
-    public int CompareTo(Book other) => -ReleaseYear.CompareTo(other.ReleaseYear);
+    public int CompareTo(Book other)
+    {
+        var comp = Author.CompareTo(other.Author);
+        if (comp == 0) return ReleaseYear.CompareTo(other.ReleaseYear);
+        return comp;
+    }
     public override string ToString() => $"{Name}, {Author}, {ReleaseYear}";
 }
