e784a95c24fb11536fe123dbda0b93b505629045
Vlad119 <unstarkble12@gmail.com>
2025-08-25 13:43:47 +0300

    Решил задачу 22_6_1

---
+++ Main/Main/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Book : IEquatable<Book>
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int ReleaseYear { get; set; }
+
+    public bool Equals(Book other)
+    {
+        return Name == other.Name && Author == other.Author && ReleaseYear == other.ReleaseYear;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, {Author}, {ReleaseYear}";
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,48 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        Book book1 = new Book
+        {
+            Name = "Предел",
+            Author = "Сергей Лукьяненко",
+            ReleaseYear = 2021
+        };
 
+        Book book2 = new Book
+        {
+            Name = "Предел",
+            Author = "Сергей Лукьяненко",
+            ReleaseYear = 2021
+        };
+
+        Console.WriteLine(book1.Equals(book2));
+
+        Book book3 = new Book
+        {
+            Name = "Предел2",
+            Author = "Сергей Лукьяненко",
+            ReleaseYear = 2021
+        };
+        Console.WriteLine(book1.Equals(book3));
+
+
+        Book book4 = new Book
+        {
+            Name = "Предел",
+            Author = "Сергей Лукьяненко2",
+            ReleaseYear = 2021
+        };
+        Console.WriteLine(book1.Equals(book4));
+
+        Book book5 = new Book
+        {
+            Name = "Предел",
+            Author = "Сергей Лукьяненко",
+            ReleaseYear = 2026
+        };
+        Console.WriteLine(book1.Equals(book5));
     }
 }
