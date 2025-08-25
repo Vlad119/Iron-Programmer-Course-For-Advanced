d1a67d8acc37655bdb5006c16c3e914d0b428251
Vlad119 <unstarkble12@gmail.com>
2025-08-25 13:49:28 +0300

    Решил задачу 22_6_2

---
+++ Main/Main/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Product : IEquatable<Product>
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Category { get; set; }
+    public Product(string name, int price, string category)
+    {
+        Name = name;
+        Price = price;
+        Category = category;
+    }
+
+    public override string ToString()
+    {
+        return $"Product: Название: {Name}, Цена: {Price}, Категория: {Category}";
+    }
+
+    public bool Equals(Product other)
+    {
+        return Name == other.Name && Price == other.Price
+            || Price == other.Price && Category == other.Category
+            || Name == other.Name && Category == other.Category;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Product product1 = new Product("Яблоко", 200, "Фрукты");
+        Product product2 = new Product("Помидор", 150, "Овощи");
+        Product product3 = new Product("Яблоко", 150, "Фрукты");
+        Console.WriteLine(product1.Equals(product3));
+        Console.WriteLine(product2.Equals(product3));
     }
 }
