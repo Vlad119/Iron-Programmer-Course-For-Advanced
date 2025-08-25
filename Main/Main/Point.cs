83b2950bc085c8fb40e6c713c3aa2f617f2ae6f7
Vlad119 <unstarkble12@gmail.com>
2025-08-25 14:15:11 +0300

    Решил задачу 22_6_3

---
+++ Main/Main/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Point : IEquatable<Point>
+{
+    public double Latitude { get; set; } // Широта
+    public double Longitude { get; set; } // Долгота
+    public Point(double Latitude, double Longitude)
+    {
+        this.Latitude = Latitude;
+        this.Longitude = Longitude;
+    }
+    public override string ToString()
+    {
+        return $" Координаты точки: {Latitude} широты, {Longitude} долготы";
+    }
+
+    public bool Equals(Point other)
+    {
+        return Math.Abs(Latitude - other.Latitude) <= 5 && Math.Abs(Longitude - other.Longitude) <= 5;
+    }
+}
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Point point1 = new Point(1, 2);
+        Point point2 = new Point(4, 2);
+        Point point3 = new Point(15, 5);
+        Console.WriteLine(point1.Equals(point2));
+        Console.WriteLine(point1.Equals(point3));
     }
 }
