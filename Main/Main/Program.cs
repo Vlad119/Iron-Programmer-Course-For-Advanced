5d0798a366c083c6a0fb97f885c50508d6af401c
Vlad119 <unstarkble12@gmail.com>
2025-08-28 12:02:04 +0300

    Решил задачу 24_1_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Task task = new Task()
+        {
+            Title = "Сделать pull request",
+            Description = "Но сначала отформатировать код",
+            Status = TaskStatus.Testing
+        };
+        Console.WriteLine($"Статус задачи <{task.Title}> - {task.Status}");
+        task.ChangeStatus(TaskStatus.Completed);
+        Console.WriteLine($"Статус задачи <{task.Title}> - {task.Status}");
     }
 }
---
+++ Main/Main/Task.cs
@@ -0,0 +1,11 @@
+﻿public class Task
+{
+    public string Title;
+    public string Description;
+    public TaskStatus Status;
+
+    public void ChangeStatus(TaskStatus newStatus)
+    {
+        Status = newStatus;
+    }
+}
