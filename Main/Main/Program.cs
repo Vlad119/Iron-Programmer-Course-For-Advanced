ee493f1dc4ba69b74c701edfa15c0b192a8a1074
Vlad119 <unstarkble12@gmail.com>
2025-08-28 14:04:20 +0300

    Решил задачу 24_1_6

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        GoTo(Side.East, 50);
+        GoTo(Side.South, 20);
+        GoTo(Side.West, 30);
+        Console.WriteLine("You found it!");
+    }
 
+    static void GoTo(Side side, int distance)
+    {
+        Console.WriteLine($"Go {side} for {distance} meters");
     }
 }
+public enum Side
+{
+    North, // север
+    East,  // восток
+    South, // юг
+    West   // запад
+};
