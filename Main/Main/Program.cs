72e55cd4c8439a6473902da18e34e15916bd35f0
Vlad119 <unstarkble12@gmail.com>
2025-08-28 14:16:23 +0300

    Решил задачу 24_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -2,5 +2,36 @@
 
 internal class Program
 {
-   
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ColorTranslate(Colors.Red, Languages.Russian));
+        Console.WriteLine(ColorTranslate(Colors.Red, Languages.English));
+    }
+
+    public static string ColorTranslate(Colors color, Languages language)
+    {
+        switch (color)
+        {
+            case Colors.Red: return language == Languages.Russian ? "Красный" : "Red";
+            case Colors.Black: return language == Languages.Russian ? "Черный" : "Black";
+            case Colors.Yellow: return language == Languages.Russian ? "Желтый" : "Yellow";
+            case Colors.White: return language == Languages.Russian ? "Белый" : "White";
+        }
+
+        return null;
+    }
+
+    public enum Colors
+    {
+        Red,
+        Black,
+        Yellow,
+        White
+    }
+
+    public enum Languages
+    {
+        English,
+        Russian
+    }
 }
