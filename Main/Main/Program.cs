6417e04c0e18dc34e8e8c3e189fad4bd47c0da94
Vlad119 <unstarkble12@gmail.com>
2025-08-28 14:07:12 +0300

    0

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -2,16 +2,5 @@
 
 internal class Program
 {
-    static void Main(string[] args)
-    {
-        GoTo(Side.East, 50);
-        GoTo(Side.South, 20);
-        GoTo(Side.West, 30);
-        Console.WriteLine("You found it!");
-    }
-
-    static void GoTo(Side side, int distance)
-    {
-        Console.WriteLine($"Go {side} for {distance} meters");
-    }
+   
 }
