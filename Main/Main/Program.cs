847dee47d12ca7b30742a77333b05ef3f89c925e
Vlad119 <unstarkble12@gmail.com>
2025-09-05 16:18:36 +0300

    Решил задачу 26_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,6 +4,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        var order1 = new Order(1, "Самокат", "ул. Солнечная 45", 3500, "Беседин Александр Владимирович");
+        Console.WriteLine(order1);
+    }
+}
 
+public record Order
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Address { get; init; }
+    public int TotalPrice { get; init; }
+    public string CustomerInfo { get; init; }
+    public Order(int id, string name, string address, int totalPrice, string customerInfo)
+    {
+        Id = id; 
+        Name = name; 
+        Address = address; 
+        TotalPrice = totalPrice; 
+        CustomerInfo = customerInfo;
     }
 }
