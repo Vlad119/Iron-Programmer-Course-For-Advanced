f6cd8e1fc66deb97b850a6a6433c46ed6faf6e34
Vlad119 <unstarkble12@gmail.com>
2025-09-05 12:52:56 +0300

    Решил задачу 25_2_2

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
+        var artistData = ("Пабло Пикассо", "Мона Лиза", 1503);
+        artistData.Item1 = "Леонардо да Винчи";
+        Console.WriteLine(artistData);
     }
 }
