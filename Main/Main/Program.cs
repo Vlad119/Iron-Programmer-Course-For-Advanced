93387eb80fd41a864e78039250457177fe2aa0b8
Vlad119 <unstarkble12@gmail.com>
2025-09-05 16:26:50 +0300

    Решил задачу 26_2_3

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        var ford = new Car("Ford", 120);
+        var tesla = new ElectricCar("Tesla", 150, 500);
 
+        Console.WriteLine(ford);
+        Console.WriteLine(tesla);
     }
+
+    public record Car(string Brand, int Speed);
+
+    public record ElectricCar(string Brand, int Speed, int BatteryRange) : Car(Brand, Speed);
 }
