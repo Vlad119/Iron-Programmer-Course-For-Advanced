f0ed3f5f537bc86189e851157ee28228afcf3f9a
Vlad119 <unstarkble12@gmail.com>
2025-09-05 12:50:32 +0300

    Решил задачу 25_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
+        var animals = ("собака", "кот", "корова", "курица", "лошадь");
+        string last = animals.Item5;
+        Console.WriteLine(last);
     }
 }
