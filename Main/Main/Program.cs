1fa858ada5c1b15f664aacc938acb7ef4dce705e
Vlad119 <unstarkble12@gmail.com>
2025-09-05 15:53:28 +0300

    Решил задачу 26_2_1

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -4,6 +4,26 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
+        var dog = new Pet("Альфред", "Золотистый ретривер", 3);
+        Console.WriteLine(dog);
     }
 }
+
+public record Pet(string Name, string Type, int Age);
+
+//public record Pet
+//{
+//    // Имя
+//    public string Name { get; init; }
+//    // Вид животного 
+//    public string Type { get; init; }
+//    // Возраст
+//    public int Age { get; init; }
+
+//    public Pet(string name, string type, int age)
+//    {
+//        Name = name;
+//        Type = type;
+//        Age = age;
+//    }
+//}
